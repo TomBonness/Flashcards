5b87fdac69f0a30024960ff0a768905c33ea6984
TomBonness <105823687+TomBonness@users.noreply.github.com>
2025-03-09 16:17:13 -0500

    Update Main.java
    Changes ArrayList for HashMaps and verifies the users input by checking if the term (key) already exists. If the users inputs the wrong definition to a term, the program outputs the correct term if it exists in the HashMap.

--- Main.java
+++ Main.java
@@ -1,43 +1,55 @@
 package flashcards;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Card> cardsInUse = new ArrayList<>();
+        HashMap<String, String> termToDefinition = new HashMap<>();
+        HashMap<String, String> definitionToTerm = new HashMap<>();
         System.out.println("Input the number of cards:");
         int numberOfCards = scanner.nextInt();
         scanner.nextLine();
 
         for (int i = 0; i < numberOfCards; i++) {
-            System.out.println("Card #" + (i + 1) + ":");
-            String term = scanner.nextLine();
-            System.out.println("The definition for card #" + (i + 1) + ":");
-            String definition = scanner.nextLine();
-            Card finishedCard = new Card(term, definition);
-            cardsInUse.add(finishedCard);
+            String term;
+            do {
+                System.out.println("Card #" + (i + 1) + ":");
+                term = scanner.nextLine();
+                System.out.println("The definition for card #" + (i + 1) + ":");
+                if (termToDefinition.containsKey(term)) {
+                    System.out.println("The term \"" + term + "\" already exists. Try again:");
+                }
+            } while (termToDefinition.containsKey(term));
+
+            String definition;
+            do {
+                System.out.println("The definition for card #" + (i + 1) + ":");
+                definition = scanner.nextLine();
+                if (definitionToTerm.containsKey(definition)) {
+                    System.out.println("The definition \"" + definition + "\" already exists. Try again:");
+                }
+            } while(definitionToTerm.containsKey(definition));
+
+            termToDefinition.put(term, definition);
+            definitionToTerm.put(definition, term);
         }
 
-        for (int i = 0; i < numberOfCards; i++) {
-            Card currentCard = cardsInUse.get(i);
-            System.out.println("Print the definition of \"" + currentCard.term + "\":");
-            if (scanner.nextLine().equals(currentCard.definition)) {
-                System.out.println("Correct!");
+        for (String currentTerm : termToDefinition.keySet()) {
+            String correctDefinition = termToDefinition.get(currentTerm);
+
+            System.out.println("Print the definition of \"" + currentTerm + "\":");
+            String userAnswer = scanner.nextLine();
+
+            if (userAnswer.equals(correctDefinition)) {
+                System.out.println("correct!");
+            } else if (definitionToTerm.containsKey(userAnswer)) {
+                String correctTerm = definitionToTerm.get(userAnswer);
+                System.out.println("Wrong. The right answer is \"" + correctDefinition + "\", but your definition is correct for \"" + correctTerm + "\".");
             } else {
-                System.out.println("Wrong. The right answer is \"" + currentCard.definition + "\".");
+                System.out.println("Wrong. The right answer is \"" + correctDefinition + "\".");
             }
         }
-    }
-}
-
-class Card {
-    String term;
-    String definition;
-
-    public Card(String term, String definition) {
-        this.term = term;
-        this.definition = definition;
     }
 }
